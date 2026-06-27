a736b4a0599393ab66ecbeed7e8572ada0ab38a2
ReGlaud <unemkin@gmail.com>
2026-06-27 13:34:28 +0600

    Пустой проект, готовый к работе

---
+++ README.md
@@ -0,0 +1,5 @@
+# RGCutscene
+
+## Этот мод поможет тебе создать свои катсцены.
+
+## This mod will help you create your own cutscenes.
---
+++ src/main/java/reglaud/cutscene/client/RGCutsceneClient.java
@@ -0,0 +1,10 @@
+package reglaud.cutscene.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RGCutsceneClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
